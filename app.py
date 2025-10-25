35f463212e91e0cd96d4d24a7af827109b3bfb80
Saket Srivastava <saket72740gmail.com@Sakets-MacBook-Air.local>
2025-10-25 12:37:57 +0530

    Two containers communicating with each other

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, jsonify, request
+import psycopg2
+import os
+
 import logging
 
 app = Flask(__name__)
@@ -9,12 +12,35 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+DB_HOST = os.getenv('DB_HOST', 'db') 
+DB_NAME = os.getenv('POSTGRES_DB', 'testdb')
+DB_USER = os.getenv('POSTGRES_USER', 'postgres')
+DB_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'password')
+
+def get_db_connection():
+    try:
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD
+        )
+        cur = conn.cursor()
+        cur.execute("SELECT version();")
+        version = cur.fetchone()
+        cur.close()
+        conn.close()
+        return version[0]
+    except Exception as e:
+        return str(e)
 
 @app.route('/', methods=['GET'])
 def home():
     if request.method == 'GET':
+        version = get_db_connection()
         response = {
             "message": "Hello, I am running inside a Docker container! 🚀",
+            "postgres_version": version,
             "status": "success",
             "method": "GET"
         }
